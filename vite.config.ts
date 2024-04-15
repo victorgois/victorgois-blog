45e3e5e9362210209093d68a5f4cc7f94ea54814
Victor Góis Pacheco <victorgois18@gmail.com>
2024-04-15 15:20:24 -0300

    Fix alias and compiler warning

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
+	resolve: {
+		alias: {
+			$lib: path.resolve("./src/lib/"),
+			$base: path.resolve("./src/baseApp")
+		}
+	},
 	plugins: [sveltekit()],
 	server: {
-		host: "0.0.0.0" // Escutar em todos os endereços IP disponíveis
+		host: "0.0.0.0"
 	}
 });
