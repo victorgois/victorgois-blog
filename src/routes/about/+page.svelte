1a114eb78d97c0a4c1c79305391fef871f7b6aeb
Victor Góis <victorgois18@gmail.com>
2024-09-12 11:11:14 -0300

    Change about page content

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,70 +1,145 @@
 <script>
-	import skills from "$lib/Skills";
-	import Timeline from "$lib/components/Timeline.svelte";
+	import { locale, t } from "../../i18n";
+	import Timeline from "../../lib/components/Timeline.svelte";
+	import TableauGraphs from "../../lib/components/TableauGraphs.svelte";
 	import { reveal } from "svelte-reveal";
-	import { t, locale } from "../../i18n";
+	import { derived } from "svelte/store";
+
+	import { visualizations } from "../../lib/store";
+	const menuItems = [0, 1];
+
+	const menuContent = derived([t], ([$t]) => [
+		{
+			Title: $t("projectPageContent.overviewTitle"),
+			Content: $t("projectPageContent.overview")
+		},
+		{
+			Title: $t("projectPageContent.academicBackgroundTitle"),
+			Content: $t("projectPageContent.academicBackground")
+		}
+	]);
+
+	/**
+	 * @type {number | null}
+	 */
+
+	let selectedMenuItem = null;
+	let showTimeline = false;
+
+	const handleClick = (/** @type {number} */ item) => {
+		selectedMenuItem = item;
+	};
+
+	const toggleTimeline = () => {
+		showTimeline = !showTimeline;
+	};
 </script>
 
 <svelte:head>
-	<title>Victor Góis — About</title>
+	<title>Victor Góis — Projects</title>
 </svelte:head>
+<main>
+	<div class="column">
+		<div class="row">
+			<div class="menuContent">
+				{#if selectedMenuItem !== null}
+					<div use:reveal={{ transition: "fade" }} class="content">
+						<h4>{$menuContent[selectedMenuItem].Title}</h4>
 
-<div class="container">
-	<main>
-		<h1 class="title">About</h1>
-		<div class="infographic" use:reveal>
-			<h1>{$t("homepage.infographic")}</h1>
-			<Timeline />
+						{#if selectedMenuItem !== 3}
+							<p use:reveal={{ transition: "fade" }}>
+								{@html $menuContent[selectedMenuItem].Content}
+							</p>
+							{#if showTimeline}
+								<Timeline />
+							{/if}
+						{:else}
+							<p use:reveal={{ transition: "fade" }}>
+								{#each Object.values(visualizations) as { title, slug }}
+									<li><a href={`projects/visualizations/${slug}`}>{title}</a></li>
+								{/each}
+							</p>
+							<TableauGraphs />
+						{/if}
+					</div>
+				{:else}
+					<div class="content">
+						<h4>{$menuContent[0].Title}</h4>
+						<p use:reveal={{ transition: "fade" }}>{@html $menuContent[0].Content}</p>
+					</div>
+				{/if}
+			</div>
 		</div>
-		<p class="footnote" use:reveal={{ transition: "fade" }}>
-			{$t("homepage.footnote")}
-		</p>
-	</main>
-</div>
+	</div>
+</main>
 
 <style>
-	.container {
-		max-width: 900px;
-		padding: 0;
-		display: flex;
-		justify-content: center;
+	.timeline-button {
+		text-align: center;
+		background-color: var(--backgroundColor);
+		color: var(--mainColor);
+		border: none;
 	}
 
 	main {
-		max-width: 100%;
-		min-height: 500px;
-		text-align: left;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	h1 {
-		font-weight: 700;
-		margin: 0 0 50px 0;
-		font-size: 36px;
+		padding: 0;
 		text-align: start;
-	}
-
-	h2 {
-		margin-top: 50px;
-	}
-
-	.list {
+		padding: 20px;
 		display: flex;
 		flex-direction: column;
-		gap: 30px;
+		justify-content: center;
+		align-items: center;
 	}
 
-	.infographic {
-		text-align: center;
+	.column {
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		max-width: 700px;
 	}
 
-	@media (min-width: 900px) {
-		.list {
-			flex-direction: row;
-		}
-		main > h1 {
-			font-size: 48px;
-		}
+	.row {
+		display: flex;
+		flex-direction: row;
+	}
+
+	.menuWrapper {
+		display: flex;
+		flex-grow: 0;
+		flex-basis: 40%;
+	}
+
+	.menuWrapper ul {
+		margin-right: 2em;
+		list-style-type: none;
+		padding: 0;
+	}
+
+	.menuContent {
+		flex-grow: 0;
+		flex-basis: 60%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+	}
+
+	button {
+		font-weight: 700;
+		padding: 0.25em;
+		text-align: start;
+		margin: 0 auto;
+		cursor: pointer;
+		font-size: 18px;
+		background-color: var(--mainColor);
+		color: var(--backgroundColor);
+	}
+
+	li {
+		margin: 0.5em;
+		list-style: none;
+	}
+
+	.content {
+		margin-left: 10px;
 	}
 </style>
