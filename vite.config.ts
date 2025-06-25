4e20b65232c0338ab46498341935dde7c67140a2
Victor Gois <victorgois18@gmail.com>
2025-06-25 19:46:37 -0300

    Fix build problems

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,8 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
 		host: "0.0.0.0" // Escutar em todos os endereços IP disponíveis
+	},
+	optimizeDeps: {
+		exclude: ['@sveltejs/kit']
 	}
 });
