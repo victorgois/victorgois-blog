91826afeab9d486b14f496df7ff27359c31ba7eb
Victor Gois <victorgois18@gmail.com>
2026-02-26 16:06:40 -0300

    Edit svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,26 @@
 import adapter from "@sveltejs/adapter-netlify";
 import { vitePreprocess } from "@sveltejs/kit/vite";
+import { mdsvex } from "mdsvex";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+/** @type {import('mdsvex').MdsvexOptions} */
+const mdsvexOptions = {
+	extensions: [".svx", ".md"],
+	rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings]
+};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: [".svelte", ".svx", ".md"],
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
